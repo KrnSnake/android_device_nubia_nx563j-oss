519ea61c26e176cd7aeb44e232610e7d7daad237
chenzc <dazzworld@163.com>
2019-11-24 11:48:19 +0800

    nx563j: Add shim for NubiaImageAlgorithm
    (cherry picked from commit 1ba01684abfa53b341882ea81953056bb4d2adf2)

--- device.mk
+++ device.mk
@@ -45,6 +45,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libNubiaImageAlgorithmShim
+
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_jdi_r63452_1080p_5p5_mipi_cmd_panel.xml
