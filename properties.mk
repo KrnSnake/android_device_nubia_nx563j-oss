1c9be3fe233b1e0392cfac25b70f6c5c89574d34
Cyborg2017 <wangyang0987654@gmail.com>
2022-09-02 21:16:09 +0800

    nx563j: Import device specific camera props

--- properties.mk
+++ properties.mk
@@ -1,21 +1,24 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Cabl
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=2 \
+    ro.vendor.display.cabl=2
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.bokeh.switch.lux=290 \
-    persist.vendor.camera.HAL3.enabled=1 \
-    persist.vendor.camera.depth.focus.cb=0 \
-    persist.vendor.camera.exif.make=Xiaomi \
-    persist.vendor.camera.expose.aux=1 \
-    persist.vendor.camera.imglib.usefdlite=1 \
-    persist.vendor.camera.isp.clock.optmz=0 \
-    persist.vendor.camera.isp.turbo=1 \
-    persist.vendor.camera.linkpreview=0 \
-    persist.vendor.camera.preview.ubwc=0 \
-    persist.vendor.camera.stats.test=0 \
-    persist.vendor.imx376_ofilm.light.lux=275 \
-    persist.vendor.imx376_ofilm.low.lux=290 \
-    persist.vendor.imx376_sunny.light.lux=275 \
-    persist.vendor.imx376_sunny.low.lux=290
-
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.sf.lcd_density=440
+    ro.total.camera.number=3
