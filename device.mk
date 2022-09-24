0da08617ac78434aa60415aefbe54cca8e819b9f
Cyborg2017 <wangyang0987654@gmail.com>
2022-09-24 19:08:09 +0800

    nx563: enable device package overlays

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 -include $(LOCAL_PATH)/properties.mk
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
