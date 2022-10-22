36d6715d7567b6a9cb32d13fb37d16cffa77239a
Cyborg2017 <wangyang0987654@gmail.com>
2022-10-23 01:22:23 +0800

    Revert "nx563j: Switch to standalone extract utils"
    This reverts commit 0105f0a58509ab0b58d2b2c251df627944339032.

--- extract-files.sh
+++ extract-files.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 function blob_fixup() {
@@ -24,15 +33,17 @@ function blob_fixup() {
         ;;
     esac
 }
-
-if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
-    return
-fi
-
 set -e
 
 export DEVICE=nx563j
-export DEVICE_COMMON=msm8998-common
 export VENDOR=nubia
+export DEVICE_BRINGUP_YEAR=2022
+export DEVICE_COMMON=msm8998-common
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+./../../$VENDOR/$DEVICE_COMMON/extract-files.sh "$@"
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
