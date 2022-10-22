148afdfc58fefe0a445557b301b161603ecf5874
Cyborg2017 <wangyang0987654@gmail.com>
2022-10-23 01:24:25 +0800

    nx563j: Add lcd density to prop

--- properties.mk
+++ properties.mk
@@ -1,8 +1,9 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.total.camera.number=3 \
-    persist.vendor.dualcam.lpm.enable=1
+    persist.vendor.dualcam.lpm.enable=1 \
+    ro.sf.lcd_density=480
 
 # Button (hide navigation bar)
 PRODUCT_PROPERTY_OVERRIDES += \
-qemu.hw.mainkeys=1
+   qemu.hw.mainkeys=1
